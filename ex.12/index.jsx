3ec338a682f6646443eb9fab8986ad476e0cdc78
Rodrigo Ribeiro <ribeiro.rodrigo1989@gmail.com>
2018-10-03 08:03:05 -0300

    exercicio12

---
+++ ex.12/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react'; 
+import ReactDOM from 'react-dom'; 
+import { combineReducers, crateStore, createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+import Field from './field'
+
+const reducers = combineReducers({
+    field: () => ({value: 'Opa'})
+})
+
+
+ReactDOM.render(
+    <Provider store={createStore(reducers)}>
+        <Field initialValue='Teste'/> 
+    </Provider>
+    , 
+    document.getElementById('app')
+) 
+
+    
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const webpack = require('webpack'); 
 
 module.exports = {
-    entry: './ex.11/index.jsx', 
+    entry: './ex.12/index.jsx', 
     output: {
         path: __dirname + '/public',
         filename: './bundle.js'
